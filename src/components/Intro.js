b3a02679a7df3be915c31a6b10640d525dd60df1
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-03-07 14:46:39 -0600

    intro title change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,7 @@ function Intro(props) {
     <div className="intro--container">
       <div className="intro--display">
         <h2 className="intro--title">Quizzical</h2>
-        <p className="intro--description">Let's play a game!</p>
+        <p className="intro--description">Let's play a game!!</p>
         <button className="intro--button" onClick={props.startGame}>
           Start Quiz
         </button>
